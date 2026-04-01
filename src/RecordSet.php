sunkan/Atlas.Mapper

commit 66c8ae30bb77395b096179a2ca0634c9954972b0

Fix 8.5 deprecations

src/RecordSet.php
@@ -97,7 +97,7 @@ abstract class RecordSet implements
             $tracker = new SplObjectStorage();
         }
 
-        if (! $tracker->contains($this)) {
+        if (! $tracker->offsetGet($this)) {
             $tracker[$this] = [];
             $array = [];
             foreach ($this as $key => $record) {

src/Mapper.php
@@ -216,11 +216,11 @@ abstract class Mapper
             $tracker = new SplObjectStorage();
         }
 
-        if ($tracker->contains($record)) {
+        if ($tracker->offsetExists($record)) {
             return;
         }
 
-        $tracker->attach($record);
+        $tracker->offsetSet($record);
 
         $this->relationships->persistBeforeNative($record, $tracker);
         $this->relationships->fixNativeRecord($record);

src/Record.php
@@ -89,7 +89,7 @@ abstract class Record implements JsonSerializable
             $tracker = new SplObjectStorage();
         }
 
-        if (! $tracker->contains($this)) {
+        if (! $tracker->offsetExists($this)) {
             $tracker[$this]
                 = $this->row->getArrayCopy($tracker)
                 + $this->related->getArrayCopy($tracker);

src/Related.php
@@ -73,7 +73,7 @@ class Related
             $tracker = new SplObjectStorage();
         }
 
-        if (! $tracker->contains($this)) {
+        if (! $tracker->offsetExists($this)) {
             $tracker[$this] = [];
             $array = [];
             foreach ($this->fields as $field => $foreign) {

commit c1ccb87d0a85c35f241a295ed8015984241126c9

Fix accidental bug introduced by 66c8ae30b

src/RecordSet.php
@@ -97,7 +97,7 @@ abstract class RecordSet implements
             $tracker = new SplObjectStorage();
         }
 
-        if (! $tracker->offsetGet($this)) {
+        if (! $tracker->offsetExists($this)) {
             $tracker[$this] = [];
             $array = [];
             foreach ($this as $key => $record) {